bb23ed919a29f7674051b2595f629dc45cf5a15b
Lucas Tostée <lucas.tostee@gmail.com>
2020-04-01 13:46:56 +0200

    Update docusaurus config

---
+++ docusaurus.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'Bizzy documentation',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'luctst', // Usually your GitHub org/user name.
+  projectName: 'bizzy-docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'bizzy',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      }
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Bizzy-docs, Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routebasePath: "",
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
